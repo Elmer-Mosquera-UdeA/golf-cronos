81d43025e6c72868c8425b4de94ae0b61efcd9ae
Nataly Orozco <natalyorozco550@gmail.com>
2026-06-06 01:26:35 -0500

    Actualizar fondo e includes en nivel_d.cpp

--- src/escenario/juego_2/nivel_d.cpp
+++ src/escenario/juego_2/nivel_d.cpp
@@ -6,18 +6,18 @@
 #include "items/personaje.h"
 #include "items/ruedapinchos.h"
 #include "items/pelotaroja.h"
-#include "agenteia.h"
-#include "bandera.h"
-#include "centrodepoder.h"
-#include "cratergris.h"
-#include "craterhielo.h"
-#include "craterrojo.h"
-#include "cronometrotiempo.h"
-#include "logrosvidas.h"
-#include "momentopelota.h"
-#include "pelotablanca.h"
-#include "portal.h"
-#include "remolinometa.h"
+#include "items/agenteia.h"
+#include "items/bandera.h"
+#include "items/centrodepoder.h"
+#include "items/cratergris.h"
+#include "items/craterhielo.h"
+#include "items/craterrojo.h"
+#include "items/cronometrotiempo.h"
+#include "items/logrosvidas.h"
+#include "items/momentopelota.h"
+#include "items/pelotablanca.h"
+#include "items/portal.h"
+#include "items/remolinometa.h"
 
 // CONSTRUCTOR
 Nivel_D::Nivel_D(QObject *parent)
@@ -25,7 +25,7 @@ Nivel_D::Nivel_D(QObject *parent)
 {
     setSceneRect(0, 0, 1366, 768);
 
-    QPixmap imagenFondo(":/resources/imgs/fondo_juego_m_u.png");
+    QPixmap imagenFondo(":/resources/imgs/fondo-sin-laberinto2");
     if (!imagenFondo.isNull()) {
         fondoEscalado = imagenFondo.scaled(1366, 768, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
     }
@@ -44,6 +44,59 @@ Nivel_D::Nivel_D(QObject *parent)
     RuedaPinchos *rueda2 = new RuedaPinchos;
     rueda2->setPos(100,200);
     this->addItem(rueda2);
+
+    AgenteIA *agenteia = new AgenteIA;
+    agenteia->setPos(100,200);
+    this->addItem(agenteia);
+
+    Bandera *bandera = new Bandera;
+    bandera->setPos(100,200);
+    this->addItem(bandera);
+
+    CentroDePoder *centrodepoder = new CentroDePoder;
+    centrodepoder->setPos(100,200);
+    this->addItem(centrodepoder);
+
+    CraterGris *cratergris = new CraterGris;
+    cratergris->setPos(100,200);
+    this->addItem(cratergris);
+
+    CraterHielo *craterhielo = new CraterHielo;
+    craterhielo->setPos(100,200);
+    this->addItem(craterhielo);
+
+    CraterRojo *craterrojo = new CraterRojo;
+    craterrojo->setPos(100,200);
+    this->addItem(craterrojo);
+
+    CronometroTiempo *cronometrotiempo = new CronometroTiempo;
+    cronometrotiempo->setPos(100,200);
+    this->addItem(cronometrotiempo);
+
+    LogrosVidas *logrosvidas = new LogrosVidas;
+    logrosvidas->setPos(100,200);
+    this->addItem(logrosvidas);
+
+    MomentoPelota *momentopelota = new MomentoPelota;
+    momentopelota->setPos(100,200);
+    this->addItem(momentopelota);
+
+    PelotaBlanca *pelotablanca = new PelotaBlanca;
+    pelotablanca->setPos(100,200);
+    this->addItem(pelotablanca);
+
+    Portal *portal = new Portal;
+    portal->setPos(100,200);
+    this->addItem(portal);
+
+    RemolinoMeta *remolinometa = new RemolinoMeta;
+    remolinometa->setPos(100,200);
+    this->addItem(remolinometa);
+
+    Personaje *personaje = new Personaje;
+    personaje->setPos(100,200);
+    this->addItem(personaje);
+
 
 
 
